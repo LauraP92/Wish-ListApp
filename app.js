60f7696a3362835b89c4156b7d421e1f2c87a8b5
LauraP92 <panalauracristina@outlook.com>
2019-12-24 13:15:21 +0000

    Final Home page and Wish List page

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 var data = {
     badKids: [
-        { name: "Andrei", toys: "car" },
-        { name: "Cami", toys: "make-up" },
-        { name: "Vali", toys: "wine" }
+        { id: 0, name: "Andrei" },
+        { id: 1, name: "Cami" },
+        { id: 2, name: "Vali" }
     ],
     goodKids: [
-        { name: "Laura", toys: "doll" },
-        { name: "Ionut", toys: "phone" },
-        { name: "Bogdan", toys: "books" }
+        { id: 3, name: "Laura" },
+        { id: 4, name: "Ionut" },
+        { id: 5, name: "Bogdan" }
     ]
 }
+
+var addBadKid = document.querySelector(".newbadKid");
+var addGoodKid = document.querySelector(".newgoodKid");
+const regex = /^[a-zA-Z]+/;
 
 showBadKids();
 showGoodKids();
 
-function showBadKids () {
+function showBadKids() {
     for (i = 0; i < data.badKids.length; i++) {
         var bad = data.badKids[i].name;
+        var kidId = data.badKids[i].id;
         $("#tablebadkids").append(
             `<tr id="${i}">
-            <td><a href="indexwish.html?kidName=${bad}">${bad}</a></td>
-            <td><button class="btn1" type="submit"><i class="material-icons right">chevron_right</i><td>
-            </button>
+            <td><a href="indexwish.html?kidId=${kidId}&name=${bad}">${bad}</a><button class="btn1 btn btn-primary" type="submit"><i class="fa fa-angle-right"></i></button></td>
             </tr>`)
     }
+
     $("td").on("click", ".btn1", function (e) {
         var id = $(this).closest('tr').attr('id');
         data.goodKids.splice(id, 0, data.badKids.splice(id, 1)[0]);
@@ -32,16 +36,31 @@ function showBadKids () {
         showGoodKids();
     })
 }
-    
-function showGoodKids () {
+
+// Add new bad kids to the list
+$(".newbadKid").keypress(function (event) {
+    if (event.which === 13 && regex.test(addBadKid.value)) {
+        // add new kid
+        var newbadKid = $(this).val();
+        $(this).val("");
+        data.badKids.push({id: data.badKids.length + data.goodKids.length, name: newbadKid});
+        $("tr").remove();
+        showBadKids();
+        showGoodKids();
+    }
+})
+
+
+function showGoodKids() {
     for (j = 0; j < data.goodKids.length; j++) {
         var good = data.goodKids[j].name;
+        var kidId = data.goodKids[j].id;
         $("#tablegoodkids").append(
             `<tr id="${j}">
-            <td><button class="btn2" type="submit"><i class="material-icons right">chevron_left</i></button><td>
-            <td><a href="indexwish.html?kidName=${good}">${good}</a></td>
+            <td><button class="btn2 btn btn-primary" type="submit"><i class="fa fa-angle-left"></i></button><a href="indexwish.html?kidId=${kidId}&name=${good}">${good}</a></td>
             </tr>`)
     }
+    
     $("td").on("click", ".btn2", function (e) {
         var id = $(this).closest('tr').attr('id');
         data.badKids.splice(id, 0, data.goodKids.splice(id, 1)[0]);
@@ -50,6 +69,21 @@ function showGoodKids () {
         showBadKids();
     })
 }
+
+// Add new good kids to the list
+$(".newgoodKid").keypress(function (event) {
+    if (event.which === 13 && regex.test(addGoodKid.value)) {
+        // add new kid
+        var newgoodKid = $(this).val();
+        $(this).val("");
+        data.goodKids.push({id: data.badKids.length + data.goodKids.length, name: newgoodKid});
+        $("tr").remove();
+        showBadKids();
+        showGoodKids();
+    }
+})
+
+
 
 
 
--- appwish.js
+++ appwish.js
@@ -1,30 +1,95 @@
-var parsedUrl = new URL(window.location.href);
-console.log(parsedUrl.searchParams.get("kidName"));
-// var kidName;
+// Split routes for username and repository name
+let kidId = "";
+let name = "";
 
-var data = {
-    badKids: [
-        { name: "Andrei", toys: ["car", "apples", "chips"] },
-        { name: "Cami", toys: "make-up" },
-        { name: "Vali", toys: "wine" }
-    ],
-    goodKids: [
-        { name: "Laura", toys: "doll" },
-        { name: "Ionut", toys: "phone" },
-        { name: "Bogdan", toys: "books" }
-    ]
+function splitRoutes() {
+    const route = window.location.search.replace("?", "").trim();
+    const params = route.split("&");
+    params.forEach(param => {
+        const fieldAndValue = param.split("=");
+        if (fieldAndValue[0] == "kidId") {
+            kidId = fieldAndValue[1]
+        }
+
+        if (fieldAndValue[0] == "name") {
+            name = fieldAndValue[1]
+        }
+
+    });
+}
+splitRoutes()
+console.log(kidId);
+console.log(name);
+
+var data = [
+    { id: 0, toys: ["car", "apples", "chips"] },
+    { id: 1, toys: ["make-up", "doll"] },
+    { id: 2, toys: ["wine", "ball"] },
+    { id: 4, toys: ["phone", "cocos"] },
+    { id: 3, toys: ["doll", "banana"] },
+    { id: 5, toys: ["books", "caju"] }
+]
+
+var index = data.map(o => o.id).indexOf(Number(kidId));
+if (index === -1) {
+    data.push({ id: Number(kidId), toys: [] })
+    index = data.map(o => o.id).indexOf(Number(kidId))
+}
+console.log(index, data[index]);
+
+$(".title").append(`<h2><a href="index.html">${name}'s List</a></h2>`)
+
+showToys();
+
+// Show toys for selected kid
+function showToys() {
+    for (i = 0; i < data[index].toys.length; i++) {
+        var toy = data[index].toys[i];
+        $(".list-group").append(`<li class="list-group-item"><span class="delete"><i class="fa fa-trash"></i></span><span class="toyName">${toy}</span><input class="toyInputEdit invisible" type="text"><button type="button" class="btn btn1 btn-outline-success">Edit</button><button type="button" class="btn btn2 btn-outline-success invisible">Save</button></li>`)
+    }
+
+    // Toy edit button
+    $(".btn1").on("click", function () {
+        $($(this).parent().children()[4]).toggleClass("invisible");
+        $($(this).parent().children()[2]).toggleClass("invisible");
+        $($(this).parent().children()[1]).toggleClass("invisible");
+        var toyName = $(this).parent().children()[1].textContent;
+        $(this).parent().children()[2].value = toyName;
+        $($(this).parent().children()[3]).toggleClass("invisible");
+        $(".toyInputEdit").keypress(function (event) {
+            if (event.which === 13) {
+                data[index].toys.splice(data[index].toys.indexOf(toyName), 1, $(this).parent().children()[2].value);
+                $(".list-group").children().remove();
+                showToys();
+            }
+        })
+        // Save toy edit button
+        $(".btn2").on("click", function () {
+            data[index].toys.splice(data[index].toys.indexOf(toyName), 1, $(this).parent().children()[2].value);
+            $(".list-group").children().remove();
+            showToys();
+        })
+    })
+
+    // Delete toys from list
+    $(".delete").on("click", function () {
+        var deletedToy = $(this).parent().children()[1].textContent;
+        var deletedToyIndex = data[index].toys.indexOf(deletedToy);
+        console.log(deletedToyIndex);
+        data[index].toys.splice(deletedToyIndex, 1);
+        $(".list-group").children().remove();
+        showToys();
+    })
 }
 
-// var index = data.badKids.map(o => o.name).indexOf(name);
-// console.log(index);
-
-
-
-// var list = data.badKids.name[kidname];
-// for (i = 0; i < data.badKids.length; i++) {
-//     for (j = 0; j < data.badKids.length; j++) {
-//     var toys = data.badKids[0].toys[j];
-//     $(".list-group").append(
-//         `<li class="list-group-item">${toys}</li>`)
-// }
-// }
+// Add new toys to the list
+$(".newToy").keypress(function (event) {
+    if (event.which === 13) {
+        // add new toys
+        var newToy = $(this).val();
+        $(this).val("");
+        data[index].toys.push(newToy);
+        $(".list-group").children().remove();
+        showToys();
+    }
+})
